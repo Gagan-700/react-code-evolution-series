e87dc53915bcf86e497d43fb7b12d17593b68e0d
Gagan-700 <gagan.jaiswal2@gmail.com>
2021-05-20 19:14:05 +0530

    Initial commit

---
+++ README.md
@@ -0,0 +1,3 @@
+# react-code-evolution-series
+
+[Edit on StackBlitz ⚡️](https://stackblitz.com/edit/react-code-evolution-series)
---
+++ src/eventBind.js
@@ -0,0 +1,8 @@
+import React from 'react';
+
+class EventBind extends React.Component {
+  render() {
+    return <h1> Hello from event bind</h1>;
+  }
+}
+export default EventBind;
